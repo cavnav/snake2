f3b2005fcef3e076b0e99406eed9138e69603948
j0101 <cavnav@gmail.com>
2019-05-26 13:28:39 +0300

    6

--- index.js
+++ index.js
@@ -1,65 +1,46 @@
-/*
- игровое поле.
- змейка.
- пища.
- очки.
- 
- перемещение змейки по полю.
- столкновение змейки со стенками.
- столкновение змейки со своим телом.
- поедание пищи.
- рост змейки.
- начисление очков.
- конец игры.
- 
-  
- 
- 
- */
 (() => {
-  gameField = {
-    width: 400,
-    height: 400
-  };
-
   class Food {
     coords = {};
-    width = 10;
-    height = 10;
   }
 
   class Snake {
-    head = {
-      x: 100,
-      y: 100
-    };
+    headXY = {};
     length = 3;
-    segment = 10; // 10x10
     move = {};
     body = [];
   }
 
   class Painter {
-    constructor(id) {
+    constructor(id, field) {
       this.canvasElement = document.getElementById(id);
       this.canvasContext = this.canvasElement.getContext('2d');
-      this.canvasElement.width = gameField.width;
-      this.canvasElement.height = gameField.height;
+      this.canvasElement.width = field.width;
+      this.canvasElement.height = field.height;
     }
   }
 
   class Game {
+    field = {
+      width: 400,
+      height: 400,
+      segment: 20,
+      segmentGap: 18
+    };
+    speed = 1000 / 15;
     snake = new Snake();
     food = new Food();
-    painter = new Painter('gameField').canvasContext;
+    painter = new Painter('gameField', this.field).canvasContext;
 
     constructor(gameFieldElement) {
       const canvasElement = document.getElementById('gameField');
       const snake = this.snake;
-      snake.head = this.getRandomPlace();
+      const food = this.food;
+
+      snake.headXY = this.getRandomPlace();
+      food.coords = this.getRandomPlace();
       snake.move = this.getRandomSnakeMove();
 
-      document.addEventListener('onkeydown', this.onkeydown);
+      document.addEventListener('keydown', this.onKeydown);
 
       setTimeout(this.timer);
     }
@@ -68,42 +49,50 @@
       const values = [-1, 1];
       const coords = ['x', 'y'];
       const randomIndex = Math.floor(Math.random() * 2);
+      const randomCoord = Math.floor(Math.random() * 2);
       return {
         ...{ x: 0, y: 0 },
-        [coords[randomIndex]]: values[randomIndex]
+        [coords[randomCoord]]: values[randomIndex]
       };
     }
 
     snakeMove() {
-      const { head, move, segment, body, length } = this.snake;
-      const { x: foodX, y: foodY } = this.food;
+      const snake = this.snake;
+      const { headXY, move, length } = snake;
+      const {
+        coords: { x: foodX, y: foodY }
+      } = this.food;
       const painter = this.painter;
+      const { segment, segmentGap } = this.field;
 
-      head.x += move.x;
-      head.y += move.y;
+      headXY.x += move.x;
+      headXY.y += move.y;
 
-      body.push(head);
+      snake.body.push({ ...headXY });
 
       painter.fillStyle = 'lime';
-      if (body.length > length) {
-        body.slice(1).map(part => {
-          painter.fillRect(
-            part.x * segment,
-            part.y * segment,
-            segment - 2,
-            segment - 2
-          );
-        });
+      if (snake.body.length > length) {
+        snake.body = snake.body.slice(1);
       }
 
-      if (head.x === foodX && head.y === foodY) {
+      snake.body.map(part => {
+        painter.fillRect(
+          part.x * segment,
+          part.y * segment,
+          segmentGap,
+          segmentGap
+        );
+      });
+
+      if (headXY.x === foodX && headXY.y === foodY) {
+        this.snake.length++;
         this.food.coords = this.getRandomPlace();
       }
     }
 
     timer = () => {
       const painter = this.painter;
-      const { width, height } = gameField;
+      const { width, height } = this.field;
 
       painter.fillStyle = 'black';
       painter.fillRect(0, 0, width, height);
@@ -111,20 +100,21 @@
       this.paintFood();
       this.snakeMove();
 
-      setTimeout(this.timer, 1000 / 15);
+      setTimeout(this.timer, this.speed);
     };
 
     paintFood() {
       const {
-        coords: { x, y },
-        width,
-        height
+        coords: { x, y }
       } = this.food;
+      const { segment, segmentGap } = this.field;
       this.painter.fillStyle = 'red';
-      this.painter.fillRect(x, y, width, height);
+      this.painter.fillRect(x * segment, y * segment, segmentGap, segmentGap);
     }
 
     onKeydown = ({ keyCode }) => {
+      const snake = this.snake;
+
       switch (keyCode) {
         case 37:
           snake.move = { x: -1, y: 0 };
@@ -142,9 +132,10 @@
     };
 
     getRandomPlace() {
+      const { segment } = this.field;
       return {
-        x: Math.floor(Math.random() * gameField.width),
-        y: Math.floor(Math.random() * gameField.height)
+        x: Math.floor(Math.random() * segment),
+        y: Math.floor(Math.random() * segment)
       };
     }
   }
